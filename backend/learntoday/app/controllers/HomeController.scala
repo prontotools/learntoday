97c5b5a6a5633b23c2a1c8bdc9482de901f6ce3b
Yothin M <m@yothinix.com>
2018-08-10 20:07:38 +0700

    Remove hardcoded HashMap :rocket: :koala: :sushi:

--- backend/learntoday/app/controllers/HomeController.scala
+++ backend/learntoday/app/controllers/HomeController.scala
@@ -12,12 +12,6 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-
-
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
@@ -31,16 +25,6 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def index() = Action { implicit request: Request[AnyContent] =>
     val source: String = Source.fromFile("conf/2018-08-10.json").getLines.mkString
     val json: JsValue = Json.parse(source)
-
-    val post: HashMap[String, String] = HashMap(
-      ("client_msg_id", "1645d6d2-eab6-4837-a0ea-4014556dc6e8"),
-      ("user","U06CVDTT6"),
-      ("text","#woocommerce #php\n<https://www.ibenic.com/manage-order-item-meta-woocommerce/>\nมันเกี่ยวกับพวก item meta data ของ order เช่นถ้าเราอยากเปลี่ยน text ใน order details หรือ email มันมี filter ของ woo ที่เปลี่ยนที่เดียว สามารถเปลี่ยนได้ทั้งในหน้า order กับ email"),
-      ("ts", convertTimestampToString("1533892979.000041"))
-    )
-
-    var postList = ArrayBuffer[HashMap[String, String]]()
-    postList += post
 
     Ok(Json.toJson(json))
   }
--- backend/learntoday/app/controllers/PostController.scala
+++ backend/learntoday/app/controllers/PostController.scala
@@ -12,12 +12,6 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-
-
 @Singleton
 class PostController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
