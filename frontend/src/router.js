6b366f7f19f12810432facaffaebb31b232c9996
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-10 18:34:51 +0700

    :recycle: Rename routes and components :cat2:

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Posts from './views/Posts.vue'
 
 Vue.use(Router)
 
@@ -10,14 +10,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Home
+      component: Posts
     },
     {
-      path: '/about',
+      path: '/post/:id',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/PostDetail.vue')
     }
   ]
 })
---
+++ frontend/src/views/PostDetail.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Post 1</h1>
+    <p>detail ja</p>
+  </div>
+</template>
