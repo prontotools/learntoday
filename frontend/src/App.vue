46864dd48ab4989f1249043a9469095aece09cfa
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-10 20:08:55 +0700

    :lipstick: Remove dangling separator :cat2:

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Posts</router-link> |
+      <router-link to="/">Posts</router-link>
     </div>
     <router-view/>
   </div>
