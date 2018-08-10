1393b98b2b2080cbaba9702add896a3c202eab49
Anpan <pan@prontomarketing.com>
2018-08-10 19:52:00 +0700

    Add content view presentation in detail page :imp: :pancakes:

--- frontend/src/views/PostDetail.vue
+++ frontend/src/views/PostDetail.vue
@@ -10,11 +10,21 @@
             <p>Pronto Tools</p>
           </div>
         </div>
+        <p class="content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p style="text-align: right;">
+          10 Aug 2018
+        </p>
       </a-card>
   </div>
 </template>
 
 <style scoped>
+.content {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
 .grid {
   width: 80%;
   margin: auto;
