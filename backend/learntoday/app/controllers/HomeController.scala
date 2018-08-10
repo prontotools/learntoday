6e3ba52768ce1599a4820c704afe00a8689a3505
Yothin M <m@yothinix.com>
2018-08-10 19:18:19 +0700

    Can convert Hashmap to json and convert time :rocket: :koala:

--- backend/learntoday/app/controllers/HomeController.scala
+++ backend/learntoday/app/controllers/HomeController.scala
@@ -5,21 +5,38 @@ import play.api._
 import play.api.mvc._
 import scala.collection.mutable.HashMap
 import play.api.libs.json.Json
+import scala.collection.mutable.ArrayBuffer
+import java.util.Date
+import java.text.SimpleDateFormat
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
+
+
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  def convertTimestampToString( ts:String ) : String = {
+      val df: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+      val date:String = df.format(ts.toDouble.toLong * 1000L)
+
+      return date
+  }
+
   def index() = Action { implicit request: Request[AnyContent] =>
-    val hashMap1: HashMap[String, String] = HashMap(
-      ("PD","Plain Donut"),
-      ("SD","Strawberry Donut"),
-      ("CD","Chocolate Donut")
+
+    val post: HashMap[String, String] = HashMap(
+      ("user","U06CVDTT6"),
+      ("text","#woocommerce #php\n<https://www.ibenic.com/manage-order-item-meta-woocommerce/>\nมันเกี่ยวกับพวก item meta data ของ order เช่นถ้าเราอยากเปลี่ยน text ใน order details หรือ email มันมี filter ของ woo ที่เปลี่ยนที่เดียว สามารถเปลี่ยนได้ทั้งในหน้า order กับ email"),
+      ("ts", convertTimestampToString("1533892979.000041"))
     )
 
-    Ok(Json.toJson(hashMap1))
+    var postList = ArrayBuffer[HashMap[String, String]]()
+    postList += post
+
+    Ok(Json.toJson(postList))
   }
 }
