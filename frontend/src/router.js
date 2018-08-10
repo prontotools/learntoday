614ef4d7eef1a7b260f93ddfd16d23b7d0915a79
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-10 18:42:35 +0700

    :recycle: Rename files again *0* :cat2:

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Posts from './views/Posts.vue'
+import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: Posts
+      component: Home
     },
     {
       path: '/post/:id',
