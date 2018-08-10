7f18b0d2037021649befdad35d756368429c0436
Yothin M <m@yothinix.com>
2018-08-10 18:44:24 +0700

    Add toJson function :rocket:

--- backend/learntoday/app/controllers/HomeController.scala
+++ backend/learntoday/app/controllers/HomeController.scala
@@ -3,6 +3,8 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import scala.collection.mutable.HashMap
+import play.api.libs.json.Json
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -11,14 +13,13 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    val hashMap1: HashMap[String, String] = HashMap(
+      ("PD","Plain Donut"),
+      ("SD","Strawberry Donut"),
+      ("CD","Chocolate Donut")
+    )
+
+    Ok(Json.toJson(hashMap1))
   }
 }
