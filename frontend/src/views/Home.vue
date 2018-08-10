72dc8a55a2287129e2b96aeef893f72fb7e438cd
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-08-10 19:12:06 +0700

    :sparkles: Add list :cat2:

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Posts msg="Welcome to Your Vue.js App"/>
-      <a-button type="primary">Primary</a-button>
-      <a-button>Default</a-button>
-      <a-button type="dashed">Dashed</a-button>
-      <a-button type="danger">Danger</a-button>
+    <Posts :posts="posts"/>
+    <a-button type="primary">Primary</a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="danger">Danger</a-button>
   </div>
 </template>
 
@@ -14,10 +13,34 @@
 // @ is an alias to /src
 import Posts from '@/components/Posts.vue'
 
+const posts = [
+  {
+    author: 'Gatuk',
+    tags: ['tag1', 'tag2'],
+    timestamp: new Date(),
+    message: 'wow'
+  },
+  {
+    author: 'Human',
+    tags: ['tag3', 'tag4'],
+    timestamp: new Date(),
+    message: 'woo'
+  },
+  {
+    author: 'Phawin',
+    tags: [],
+    timestamp: new Date(),
+    message: "I'm your father"
+  }
+]
+
 export default {
   name: 'home',
   components: {
     Posts
-  }
+  },
+  data: () => ({
+    posts
+  })
 }
 </script>
