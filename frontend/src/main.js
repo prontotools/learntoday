19d6ded73586fa3c57cf50efa206b93ffb023e4c
Anpan <pan@prontomarketing.com>
2018-08-10 18:58:43 +0700

    :sparkles: Add Ant desing vue lib :pancakes:

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Button from 'ant-design-vue/lib/button'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.component(Button.name, Button)
 
 Vue.config.productionTip = false
 
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,8 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Posts msg="Welcome to Your Vue.js App"/>
+      <a-button type="primary">Primary</a-button>
+      <a-button>Default</a-button>
+      <a-button type="dashed">Dashed</a-button>
+      <a-button type="danger">Danger</a-button>
   </div>
 </template>
+
 
 <script>
 // @ is an alias to /src
